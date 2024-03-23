699ec801c40719b1be880b36231f6d050c3ceb89
Mubarak kual louis <mubaraklouis@gmail.com>
2024-03-23 10:50:56 +0200

    working on building the model

--- src/data_cleaning/dataCleaning.py
+++ src/data_cleaning/dataCleaning.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 # Methods:
 #     cleanData() - this method is used to clean the data 
       
@@ -8,8 +10,11 @@ class Datacleaning:
     #@method : cleanData()
     #@param : dataset: numpy array
 
+   
+
+
 
     def cleanData(dataset):
-
-        return dataset
+        data = np.array(["kual","louis","dau","dau","arop"])
+        return data
 
